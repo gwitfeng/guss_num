a197741d1454041c2b43d38678ee7a3217fbee6d
gwitfeng <gwitfeng@gmail.com>
2018-09-11 22:34:08 +0800

    basic ver.

---
+++ guss_num.py
@@ -0,0 +1,24 @@
+# guss_num
+import random
+start = input ('請輸入亂數開始的數字 : ')
+end = input('請輸入亂數結束的數字')                      
+start = int (start)
+end = int (end)
+r = random.randint(start , end)
+count = 0
+while True:
+    count = count + 1  
+    num = input ('請猜數字 ：')
+    num = int (num)
+
+    if num == r :
+        print ('你猜對了  ！！')
+        print ('這是你猜的第' , count , '次')
+        break
+    elif num > r :
+        print ('比答案大 ！！')
+    elif num < r :
+        print ('比答案小 ！')
+print ('這是你猜的第' , count , '次')
+
+          
